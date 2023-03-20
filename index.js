a22f25001dc07b9e0459e36c06df6f7b9c3b3587
Ianm234 <126569965+Ianm234@users.noreply.github.com>
2023-03-20 12:04:29 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,75 +1,35 @@
-// Code your solution in this file!
-function (){
-console.log (distanceFromHqInBlocks(43))
-console.log (distanceFromHqInBlocks(50))
-console.log (calculateDistanceBelow42ndStreet)
+// code your solution here
+function saturdayFun(specified){
+    console.log ('This Saturday, I want to.....!')
+    let activity='roller skate'
+    console.log ('saturdayFun $(activity)!');
 }
-distanceFromHqInBlocks(43) (){distanceFromHq
-       const ="1 Block"
+saturdayFun("bathe my dog")
+
+
+function mondayWork(specified){
+    console.log ('This Monday, I will.....!')
+    let activity= 'go to the office'
+    console.log ('mondayWork $(activity)!');
 }
-distanceFromHqInBlocks(50) (){
-      const ='8 Blocks'
+mondayWork("work from home")
+
+function wrapAdjective(specified){
+    console.log ('You are')
+    let result = wrapAdjective('*')
+    let emphatic = result("a hard worker")
+    console.log ('wrapAdjective $(emphatic)!')
 }
-calculateDistanceBelow42ndStreet () {
-     console.log (distanceFromHqInBlocks(34))
-     return '8 Blocks'
-}
-function (){
-console.log (distanceFromHqInFeet(43))
-console.log (distanceFromHqInFeet(50))
-console.log (calculateDistanceBelow42ndStreet)
-}
-distanceFromHqInFeet(43) (){distanceFromHq
-       const ='264 Feet'
-}
-distanceFromHqInFeet(50) (){
-      const ='2112 Feet'
-}
-calculateDistanceBelow42ndStreet () {
-     console.log (distanceFromHqInFeet(34))
-     return '2112 Feet'
-}
-Function (){
-  console.log (distanceTravelledInFeet)
-}
-distanceTravelledInFeet (){
- console.log (distanceTravelledInFeet(50, 60))
- console.log (distanceTravelledInFeet(34,28))
- console.log (distanceTravelledInFeet(43, 48))
-}
-distanceTravelledInFeet(50, 60){
-return 2640
-}
-distanceTravelledInFeet(34, 28){
-return 1584
-}
-distanceTravelledInFeet(43, 48){
-return 1320
-}
-function (){
- console.log(calculatesFarePrice(start, destination))
-}
-calculatesFarePrice(start, destination) {
-console.log(calculatesFarePrice(43, 44))
-console.log(calculatesFarePrice(34, 32))
-console.log(calculatesFarePrice(50, 58))
-console.log(calculatesFarePrice(34, 24))
-}
-calculatesFarePrice(43, 44) {
-   return 0
-}
-calculatesFarePrice(34, 32) {
-    return 2.56 cents
-    return first-400-feet-free
-}
-calculatesFarePrice(50, 58) {
-   return 25 Dollars
-  return 'charges 25 dollars for a distance over 2000 feet'
-}
-calculatesFarePrice(34, 24){
-   return 'cannot travel that far'
-   return 'does not allow rides over 2500 feet'
-}
+function wrapAdjective(specified){
+    console.log ('You are')
+    let result = wrapAdjective("||")
+    let emphatic = result("a dedicated programmer")
+    console.log ('wrapAdjective $(emphatic)!')
+} 
+console.log(wrapAdjective)
+
+
+
 
 
 
